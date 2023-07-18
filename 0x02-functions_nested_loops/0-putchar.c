56e41a53483d9f5de407a4c8ad440ed36de0470f
Sajith Abubakar <Sajith Abubakar>
2023-07-18 16:38:29 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
